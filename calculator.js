e699e1c98bb7eacf27c722875957b36f66241855
Ivy Bao <85028586+Citrusboa@users.noreply.github.com>
2022-01-02 13:01:59 -0700

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,79 @@
+let prevVal = "";
+let newVal = "";
+let resultVal = "";
+let mathOperator = "";
+let decimalclicked = false;
+let valMemStored = "";
+
+function numButPress(num) {
+    if (resultVal) {
+        newVal = num;
+        resultVal = "";
+    } else {
+        if (num === ".") {
+            if (decimalClicked != true) {
+                newVal += num;
+                decimalClicked = true;
+            }
+        } else {
+            newVal += num;
+        }
+    }
+    document.getElementById("entry").value = newVal;
+}
+
+function mathButPress(operator) {
+    if (!resultVal) {
+        prevVal = newVal;
+    } else {
+        prevVal = resultVal;
+    }
+    newVal = "";
+    decimalClicked = false;
+    mathOperator = operator;
+    resultVal = "";
+    document.getElementById("entry").value = "";
+}
+function equalButPress(num) {
+    decimalClicked = false;
+    prevVal = parseFloat(prevVal);
+    newVal = parseFloat(newVal);
+
+    switch(mathOperator) {
+        case "+":
+            resultVal = prevVal + newVal;
+            break;
+        case "-":
+            resultVal = prevVal - newVal;
+            break;
+
+        case "*":
+            resultVal = prevVal * newVal;
+            break;
+        case "/":
+            resultVal = prevVal / newVal;
+            break;
+        default: 
+            resultVal = newVal;
+    }
+    prevVal = resultVal;
+    document.getElementById("entry").value = resultVal;
+}
+
+function clearButPress() {
+    prevVal = "";
+    newVal = "";
+    resultVal = "";
+    mathOperator = "";
+    decimalclicked = false;
+    document.getElementById("entry").value = "0";
+}
+function copyButPress(num) {
+    valMemStored = document.getElementById("entry").value;
+}
+function pasteButPress(num) {
+    if(valMemStored) {
+        document.getElementById("entry").value = valMemStored;
+        newVal = valMemStored;
+    }
+}
